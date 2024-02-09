bfe10da97a1378a38e721872bddd139f84ad609d
Fathurrohman Elkusnandi <f4thurz@gmail.com>
2024-02-09 17:42:10 +0700

    Update BottomSheetCountdown UI

--- feature/countdown/src/main/java/com/elkusnandi/untilthen/countdown/ui/component/BottomSheetCountdown.kt
+++ feature/countdown/src/main/java/com/elkusnandi/untilthen/countdown/ui/component/BottomSheetCountdown.kt
@@ -2,7 +2,10 @@ package com.elkusnandi.untilthen.countdown.ui.component
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
@@ -20,6 +23,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.elkusnandi.core.design.theme.UntilThenTheme
 import com.elkusnandi.data.countdown.model.Countdown
 import com.elkusnandi.untilthen.countdown.R
@@ -63,13 +67,19 @@ fun BottomSheetCountdown(
                 state.updateDateTime(selectedDateMillis)
             }
 
-            Column(modifier = Modifier.fillMaxWidth()) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp, vertical = 8.dp)
+            ) {
                 TextField(
                     modifier = Modifier.fillMaxWidth(),
                     value = state.titleText ?: "",
                     onValueChange = { state.updateTitle(it) },
                     label = { Text(text = stringResource(id = R.string.title)) }
                 )
+
+                Spacer(modifier = Modifier.height(8.dp))
 
                 TextField(
                     modifier = Modifier
@@ -93,21 +103,27 @@ fun BottomSheetCountdown(
                     label = { Text(text = stringResource(id = R.string.date_time)) }
                 )
 
-                Button(onClick = {
-                    scope.launch {
-                        sheetState.hide()
-                    }.invokeOnCompletion {
-                        val title = state.titleText
-                        val dateTime = state.dateTimeValue
-                        if (title != null && dateTime != null) {
-                            onCreateCountdown(title, dateTime, countdown?.id)
-                        }
-                        state.hideBottomSheet()
-                    }
+                Spacer(modifier = Modifier.height(8.dp))
 
-                }) {
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = {
+                        scope.launch {
+                            sheetState.hide()
+                        }.invokeOnCompletion {
+                            val title = state.titleText
+                            val dateTime = state.dateTimeValue
+                            if (title != null && dateTime != null) {
+                                onCreateCountdown(title, dateTime, countdown?.id)
+                            }
+                            state.hideBottomSheet()
+                        }
+
+                    }) {
                     Text(text = stringResource(id = if (countdown == null) R.string.create else R.string.update))
                 }
+
+                Spacer(modifier = Modifier.height(8.dp))
             }
         }
 
